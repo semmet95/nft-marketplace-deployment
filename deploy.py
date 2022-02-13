95be856ebb782a65715b3940b7b25236042bc49d
Amit Singh <singhamitch@gmail.com>
2022-02-13 23:31:16 +0530

    uses flask to run the deployer

--- deploy.py
+++ deploy.py
@@ -1,17 +1,22 @@
 import os
 import subprocess
 
-deployment_network = 'rinkeby'
-listing_price = '0.025'
+from flask import Flask, request
 
-cd_dir = 'cd ' + os.path.join(os.getcwd(), 'js-deployer')
-hardhat_compile = 'npx hardhat compile'
-hardhat_run = 'npx hardhat run ./scripts/deploy.js --network ' + deployment_network
+@app.route('/deploymarketplace', methods=['GET'])
+def deploy_marketplace():
 
-os.environ["REACT_APP_LISTING_PRICE"] = listing_price
-deploy_cmd = ' && '.join((cd_dir, hardhat_compile, hardhat_run))
+    deployment_network = request.args.get('network')
+    listing_price = request.args.get('listing-price')
 
-console_logs = subprocess.check_output(deploy_cmd, shell=True).decode('utf-8')
-console_logs = list(filter(None, console_logs.split('\n')))
+    cd_dir = 'cd ' + os.path.join(os.getcwd(), 'js-deployer')
+    hardhat_compile = 'npx hardhat compile'
+    hardhat_run = 'npx hardhat run ./scripts/deploy.js --network ' + deployment_network
 
-print(console_logs[-1])
+    os.environ["REACT_APP_LISTING_PRICE"] = listing_price
+    deploy_cmd = ' && '.join((cd_dir, hardhat_compile, hardhat_run))
+
+    console_logs = subprocess.check_output(deploy_cmd, shell=True).decode('utf-8')
+    console_logs = list(filter(None, console_logs.split('\n')))
+
+    print(console_logs[-1])
