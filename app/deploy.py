b9ba14d05500784bcf29c78228564ca758d0a346
Amit Singh <singhamitch@gmail.com>
2022-02-15 02:24:03 +0530

    takes contract name as get request parameter

--- app/deploy.py
+++ app/deploy.py
@@ -8,7 +8,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # flag
-contract_deployed = False
+contract_deployed = None
 console_logs = None
 
 def hardhat_cmd(deploy_cmd):
@@ -21,6 +21,37 @@ def hardhat_cmd(deploy_cmd):
 def deploy_marketplace():
     global contract_deployed, console_logs
 
+    contract_deployed = False
+
+    deployment_network = request.args.get('network')
+    listing_price = request.args.get('listing-price')
+    contract_name = request.args.get('contract-name', default='NFTMarket')
+
+    cd_dir = 'cd ' + os.path.join(os.path.dirname(os.path.dirname(__file__)), 'js-deployer')
+    hardhat_compile = 'npx hardhat compile'
+    hardhat_run = 'npx hardhat run ./scripts/deploy-marketplace.js --network ' + deployment_network
+
+    os.environ["REACT_APP_LISTING_PRICE"] = listing_price
+    deploy_cmd = ' && '.join((cd_dir, hardhat_compile, hardhat_run))
+
+    print('starting contract deployment')
+    threading.Thread(target=hardhat_cmd(deploy_cmd)).start()
+
+    while not contract_deployed:
+        print('waiting for the contract to be deployed...')
+        time.sleep(5)
+
+    console_logs = list(filter(None, console_logs.split('\n')))
+
+    nft_marketplace_address = console_logs[-1].split('=')[-1]
+
+    return nft_marketplace_address
+
+@app.route('/deploynft', methods=['GET'])
+def deploy_marketplace():
+    global contract_deployed, console_logs
+
+    contract_deployed = False
     deployment_network = request.args.get('network')
     listing_price = request.args.get('listing-price')
 
