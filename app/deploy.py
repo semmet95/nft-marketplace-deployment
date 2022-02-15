33a600a6e07b08608342de6a4f6c3ec69843f1b0
Amit Singh <singhamitch@gmail.com>
2022-02-15 17:03:51 +0530

    removes multithreading

--- app/deploy.py
+++ app/deploy.py
@@ -1,27 +1,21 @@
 import os
 import subprocess
-import threading
 import time
 
 from flask import Flask, request
 
 app = Flask(__name__)
 
-# flag
-contract_deployed = None
 console_logs = None
 
 def hardhat_cmd(deploy_cmd):
-    global contract_deployed, console_logs
+    global console_logs
 
     console_logs = subprocess.check_output(deploy_cmd, shell=True).decode('utf-8')
-    contract_deployed = True
 
 @app.route('/deploymarketplace', methods=['GET'])
 def deploy_marketplace():
-    global contract_deployed, console_logs
-
-    contract_deployed = False
+    global console_logs
 
     deployment_network = request.args.get('network')
     listing_price = request.args.get('listing-price')
@@ -36,12 +30,7 @@ def deploy_marketplace():
     deploy_cmd = ' && '.join((cd_dir, hardhat_compile, hardhat_run))
 
     print('deploying contract:', contract_name)
-    #threading.Thread(target=hardhat_cmd, args=(deploy_cmd,)).start()
     hardhat_cmd(deploy_cmd)
-
-    while not contract_deployed:
-        print('waiting for the contract to be deployed...')
-        time.sleep(2)
 
     console_logs = list(filter(None, console_logs.split('\n')))
 
@@ -51,9 +40,8 @@ def deploy_marketplace():
 
 @app.route('/deploynft', methods=['GET'])
 def deploy_nft():
-    global contract_deployed, console_logs
+    global console_logs
 
-    contract_deployed = False
     deployment_network = request.args.get('network')
     contract_name = request.args.get('contract-name', default='NFTDefault')
     market_address = request.args.get('market-address')
@@ -67,11 +55,7 @@ def deploy_nft():
     deploy_cmd = ' && '.join((cd_dir, hardhat_compile, hardhat_run))
 
     print('deploying contract:', contract_name)
-    threading.Thread(target=hardhat_cmd(deploy_cmd)).start()
-
-    while not contract_deployed:
-        print('waiting for the contract to be deployed...')
-        time.sleep(5)
+    hardhat_cmd(deploy_cmd)
 
     console_logs = list(filter(None, console_logs.split('\n')))
 
