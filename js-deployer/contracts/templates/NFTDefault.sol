fd9272c56be775304b131d616c467874bba92d6d
Amit Singh <singhamitch@gmail.com>
2022-02-15 15:39:26 +0530

    adds default nft contract

---
+++ js-deployer/contracts/templates/NFTDefault.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import "../modules/libs/Counters.sol";
+
+import "../modules/ERC721URIStorage.sol";
+
+contract NFTDefault is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor(address marketplaceAddress) ERC721("MASKONDI", "MASKT") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+    }
+}
